2fdd4c5a48dc238cb5fb61bc1d3c5dfcf48b5bc5
PranavIIITN <pranavtiwari98@gmail.com>
2026-01-27 23:52:56 +0530

    DB connection

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,42 @@
-console.log("Hello Ajay");
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error connecting to the database");
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+
+        })
+    }
+    catch (error) {
+        console.error("Error connecting to the database", error);
+        throw error;
+    }
+})()
+*/    
