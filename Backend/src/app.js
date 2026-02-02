351116d6293a4cf7441a162b6ad26d6a7dcca094
PranavIIITN <pranavtiwari98@gmail.com>
2026-02-03 00:03:11 +0530

    basic controllers

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,25 @@
-console.log("Backend index.js file loaded.");
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-console.log("test 3");
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+export { app }
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,42 +1,51 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
 
 
 
 
 
 /*
-import express from "express";
-const app = express();
-
-
-
+import express from "express"
+const app = express()
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("Error connecting to the database");
-            throw error;
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
         })
 
         app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-
+            console.log(`App is listening on port ${process.env.PORT}`);
         })
-    }
-    catch (error) {
-        console.error("Error connecting to the database", error);
-        throw error;
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
     }
 })()
-*/    
+
+*/
